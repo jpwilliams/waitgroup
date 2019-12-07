e1c2e1a513912184eda15fca916df24b49abe466
Jack Williams <jpwilliamsphotography@gmail.com>
2019-12-07 13:27:05 +0000

    🚀👨‍🚀

---
+++ index.js
@@ -0,0 +1,26 @@
+const { EventEmitter } = require('events')
+
+class WaitGroup {
+	constructor () {
+		this._current = 0
+		this._emitter = new EventEmitter()
+	}
+
+	add (delta = 1) {
+		this._current += delta
+		if (this._current === 0) this._emitter.emit('done')
+	}
+
+	done () {
+		this.add(-1)
+	}
+
+	wait () {
+		return new Promise((resolve) => {
+			if (this._current === 0) return resolve()
+			this._emitter.once('done', () => resolve())
+		})
+	}
+}
+
+module.exports = WaitGroup
