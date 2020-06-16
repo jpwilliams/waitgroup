d3128097f8ed85e544fa6e58ae1bf047566660c0
Jack Williams <jpwilliamsphotography@gmail.com>
2020-06-16 23:41:48 +0100

    move manual types to in-code types
    Much better.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,43 @@
 import { EventEmitter } from 'events'
 
-class WaitGroup {
-	_current: number
-	_emitter: EventEmitter
+/**
+ * A WaitGroup waits for a collection of actions to finish.
+ * The main goroutine calls `add` to set the number of actions to wait for.
+ * Then each of the actions runs and calls `done` when finished.
+ * At the same time, `wait` can be used to return a promise that resolves when all actions have finished.
+ *
+ * The class doesn't implement the race-condition requirements that the Golang package has due to the
+ * way Node functions, meaning `add`, `done` and `wait` can be called at any time, in any order.
+ */
+export class WaitGroup {
+	private _current = 0
+	private _emitter = new EventEmitter()
 
-	constructor () {
-		this._current = 0
-		this._emitter = new EventEmitter()
-	}
-
-	add (delta: number = 1): void {
+	/**
+	 * Adds a delta, which may be negative, to the WaitGroup counter.
+	 * If the counter becomes zero, all promises returned from `wait` are resolved.
+	 * If the counter goes negative, an error is thrown.
+	 */
+	public add (delta: number = 1): void {
 		this._current += delta
 		if (this._current < 0) throw new Error('Negative WaitGroup counter')
 		if (this._current === 0) this._emitter.emit('done')
 	}
 
-	done (): void {
+	/**
+	 * Decrements the WaitGroup counter by one.
+	 */
+	public done (): void {
 		this.add(-1)
 	}
 
-	wait (): Promise<void> {
+	/**
+	 * Returns a promise that resolves when the WaitGroup counter is zero.
+	 */
+	public wait (): Promise<void> {
 		return new Promise((resolve) => {
 			if (this._current === 0) return resolve()
 			this._emitter.once('done', () => resolve())
 		})
 	}
-}
-
-export {
-	WaitGroup
 }
