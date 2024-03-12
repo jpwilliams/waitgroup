f479a56e0c14149f346e7247d7a83aa14fc1fc49
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-03-12 23:17:18 +0000

    Clean up comments

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 /**
- * A WaitGroup waits for a collection of actions to finish.
- * The main goroutine calls `add` to set the number of actions to wait for.
- * Then each of the actions runs and calls `done` when finished.
- * At the same time, `wait` can be used to return a promise that resolves when all actions have finished.
+ * A {@link WaitGroup} waits for a collection of actions to finish. ~The main
+ * goroutine calls~ You call {@link add} to set the number of actions to wait
+ * for. Then each of the actions runs and calls {@link done} when finished. At
+ * the same time, {@link wait} can be used to return a promise that resolves
+ * when all actions have finished.
  *
- * The class doesn't implement the race-condition requirements that the Golang package has due to the
- * way Node functions, meaning `add`, `done` and `wait` can be called at any time, in any order.
+ * The class doesn't implement the race-condition requirements that the Golang
+ * package has due to the way JavaScript functions, meaning {@link add},
+ * {@link done} and {@link wait} can be called at any time, in any order.
  */
 export class WaitGroup {
 	private current = 0
@@ -26,9 +28,13 @@ export class WaitGroup {
 	}
 
 	/**
-   * Adds a delta, which may be negative, to the WaitGroup counter.
-   * If the counter becomes zero, all promises returned from `wait` are resolved.
-   * If the counter goes negative, an error is thrown.
+   * Adds a {@link delta}, which may be negative, to the {@link WaitGroup}'s
+   * {@link current} counter.
+   *
+   * If the {@link current} counter becomes zero, all promises returned from
+   * {@link wait} are resolved.
+   *
+   * If the {@link current} counter goes negative, an error is thrown.
    */
 	public add (delta: number = 1): void {
 		this.current += delta
@@ -37,15 +43,16 @@ export class WaitGroup {
 	}
 
 	/**
-   * Decrements the WaitGroup counter by one.
+   * Decrements the {@link WaitGroup}'s {@link current} counter by one.
    */
 	public done (): void {
 		this.add(-1)
 	}
 
 	/**
-   * Returns a promise that resolves when the WaitGroup counter is zero.
-   * If the counter is zero when the method is called, it's resolved immediately.
+   * Returns a promise that resolves when the {@link WaitGroup}'s
+   * {@link current} counter is zero. If the {@link current} counter is zero
+   * when the method is called, it's resolved immediately.
    */
 	public wait (): Promise<void> {
 		return new Promise((resolve) => {
